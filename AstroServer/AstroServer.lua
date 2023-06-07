846482045d2e1694cfb4d01adcafd12a4bc4e045
CodeSaviour <103268625+CodeSaviour@users.noreply.github.com>
2023-06-07 14:57:46 +0200

    Transferred to AstroServer V4
    Security measures update + better visualization

--- AstroServer/AstroServer.lua
+++ AstroServer/AstroServer.lua
@@ -1,11 +1,23 @@
+-- AstroServer V4
+-- Designed for: Enclosed Survival
+-- Credits: CodeSaviour
+
 local LoadedPlugins = {}
 
-local function warnServer(message)
-	warn(("[%s]: %s"):format(script.Name, message))
+_G.warnServer = function(message,s)
+	if s then
+		warn(("[🌎] [%s".."/"..s.."]: %s"):format(script.Name, message))
+	else
+		warn(("[🌎] [%s]: %s"):format(script.Name, message))
+	end
 end
 
-local function printServer(message)
-	print(("[%s]: %s"):format(script.Name, message))
+_G.printServer = function(message,s)
+	if s then
+		print(("[🌎] [%s".."/"..s.."]: %s"):format(script.Name, message))
+	else
+		print(("[🌎] [%s]: %s"):format(script.Name, message))
+	end
 end
 
 _G.ReturnServerModule = function(name)
@@ -18,7 +30,7 @@ end
 
 _G.WaitForServerPlugin = function(pluginName, overrideTime)
 	if not pluginName then
-		warnServer("Plugin name was not specified.")
+		_G._G.warnServer("Plugin name was not specified.")
 		return
 	end
 
@@ -31,7 +43,7 @@ _G.WaitForServerPlugin = function(pluginName, overrideTime)
 		wait()
 	until table.find(LoadedPlugins, pluginName) or tick() - startWait >= overrideTime
 	if tick() - startWait >= overrideTime then
-		warnServer(("Waiting for plugin %s ignored due to reaching override time."):format(pluginName))
+		_G.warnServer(("Waiting for plugin %s ignored due to reaching override time."):format(pluginName))
 		return false
 	end
 	if table.find(LoadedPlugins, pluginName) then
@@ -39,48 +51,54 @@ _G.WaitForServerPlugin = function(pluginName, overrideTime)
 	end
 end
 
-local function initPlugins()
-	local startTime = tick()
-	local loadedCount = 0
-	local priorityFolder
-	if script:FindFirstChild("Priority") then
-		priorityFolder = script.Priority
-		for _, moduleScript in ipairs(priorityFolder:GetDescendants()) do
-			if moduleScript:IsA("ModuleScript") and not moduleScript:GetAttribute("Override") then
+
+local function initServer()
+	local success, fail = pcall(function()
+		local startTime = tick()
+		local loadedCount = 0
+		local priorityFolder
+		if script:FindFirstChild("Priority") then
+			priorityFolder = script.Priority
+			for _, moduleScript in ipairs(priorityFolder:GetDescendants()) do
+				if moduleScript:IsA("ModuleScript") and not moduleScript:GetAttribute("Override") then
+					local moduleStartTime = tick()
+					local success, result = pcall(function()
+						local module = require(moduleScript)
+						module.init(script.Name)
+					end)
+					if not success then
+						_G.warnServer(("Error loading priority plugin '%s': %s"):format(moduleScript.Name, result))
+					else
+						loadedCount = loadedCount + 1
+						table.insert(LoadedPlugins, moduleScript.Name)
+						_G.printServer(("%s [PRIORITY] initialized (%.1fms)"):format(moduleScript.Name, (tick() - moduleStartTime) * 1000))
+					end
+				end
+			end
+		end
+
+		for _, moduleScript in ipairs(script:GetDescendants()) do
+			if moduleScript:IsA("ModuleScript") and moduleScript.Parent ~= priorityFolder and not moduleScript:GetAttribute("Override") then
 				local moduleStartTime = tick()
 				local success, result = pcall(function()
 					local module = require(moduleScript)
 					module.init(script.Name)
 				end)
 				if not success then
-					warnServer(("Error loading priority plugin '%s': %s"):format(moduleScript.Name, result))
+					_G.warnServer(("Error loading plugin '%s': %s"):format(moduleScript.Name, result))
 				else
 					loadedCount = loadedCount + 1
 					table.insert(LoadedPlugins, moduleScript.Name)
-					printServer(("%s [PRIORITY] initialized (%.1fms)"):format(moduleScript.Name, (tick() - moduleStartTime) * 1000))
+					_G.printServer(("%s initialized (%.1fms)"):format(moduleScript.Name, (tick() - moduleStartTime) * 1000))
 				end
 			end
 		end
+		
+		_G.warnServer(("Loaded %d plugins in %.1fms"):format(loadedCount, (tick() - startTime) * 1000))
+	end)
+	if fail then
+		_G.warnServer("SERVER BOOT ERROR \n"..fail)
 	end
-
-	for _, moduleScript in ipairs(script:GetDescendants()) do
-		if moduleScript:IsA("ModuleScript") and moduleScript.Parent ~= priorityFolder and not moduleScript:GetAttribute("Override") then
-			local moduleStartTime = tick()
-			local success, result = pcall(function()
-				local module = require(moduleScript)
-				module.init(script.Name)
-			end)
-			if not success then
-				warnServer(("Error loading plugin '%s': %s"):format(moduleScript.Name, result))
-			else
-				loadedCount = loadedCount + 1
-				table.insert(LoadedPlugins, moduleScript.Name)
-				printServer(("%s initialized (%.1fms)"):format(moduleScript.Name, (tick() - moduleStartTime) * 1000))
-			end
-		end
-	end
-
-	warnServer(("Loaded %d plugins in %.1fms"):format(loadedCount, (tick() - startTime) * 1000))
 end
 
-initPlugins()
+initServer()
